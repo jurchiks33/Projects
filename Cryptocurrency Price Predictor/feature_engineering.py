e942d17ff77075908ac558f62c833d00331cdd13
jurchiks33 <jurisslisans@gmail.com>
2024-07-19 19:13:29 +0200

    feature engineering

--- Cryptocurrency Price Predictor/feature_engineering.py
+++ Cryptocurrency Price Predictor/feature_engineering.py
@@ -10,3 +10,13 @@ def feature_engineering(df):
     # trade signal calculation
     df['Buy_signal_Price'] = np.where(df['MA7'] > df['MA21'], df['price'], np.nan)
     df['Sell_signal_Price'] = np.where(df['MA7'] < df['MA21'], df['price'], np.nan)
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(df['timestamp'], df['price'], label='Bitcoin Price', alpha=0.5)
+    plt.plot(df['timestamp'], df['MA7'], label='MA7', alpha=0.5)
+    plt.plot(df['timestamp'], df['MA14'], label='MA14', alpha=0.5)
+    plt.plot(df['timestamp'], df['MA21'], label='MA21', alpha=0.5)
+    plt.scatter(df['timestamp'], df['Buy_signal_Price'], label='Buy', marker='^', color='green')
+    plt.scatter(df['timestamp'], df['Sell_signal_Price'], label='Sell', marker='v', color='green')
+    plt.xlabel('Date')
+    
